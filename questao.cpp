86e334154201ff054e737830b77473d41db0c731
RhenzoHideki <rhskgames@gmail.com>
2020-03-10 10:26:00 -0300

    Primeiro commit

--- questao.cpp
+++ questao.cpp
@@ -1,1 +1,17 @@
 // implemente aqui sua função
+#include "questao.h"
+
+
+string junta(list<string> & l, const string & delim){
+
+    string r;
+    int n = l.size();
+
+    for (auto & sub: l) {
+        r += sub;
+        n--;
+        if (n>0) r += delim;
+    }
+
+    return r;
+}
--- questao.h
+++ questao.h
@@ -8,4 +8,14 @@
 // declare aqui sua função
 // não esqueça dos include !
 
+#include "questao.h"
+#include <string>
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+string junta(list<string> & l, const string & delim);
+
+
 #endif //PROG_QUESTAO_H
